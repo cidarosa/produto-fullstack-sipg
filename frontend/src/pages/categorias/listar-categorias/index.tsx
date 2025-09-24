179080676a470b1a21f2c36acf5154b60a022b4b
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-24 11:54:00 -0300

    feat: add models, categoria-service, utils, update listar categorias

--- frontend/src/pages/categorias/listar-categorias/index.tsx
+++ frontend/src/pages/categorias/listar-categorias/index.tsx
@@ -1,18 +1,36 @@
-import { Box, Typography } from "@mui/material";
-
+import {
+  Box,
+  Paper,
+  Table,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
 
 export default function ListarCategorias() {
+  return (
+    <Box sx={{ p: 4 }}>
+      <Typography variant="h4" component="h1" gutterBottom>
+        Página de Listagem de Categorias
+      </Typography>
 
-    return(
-
-        <Box sx={{ p: 4}}>
-            <Typography variant="h4" component='h1' gutterBottom>
-                Página de Listagem de Categorias
-            </Typography>
-
-            <Typography variant="body1">
-                Aqui será a lista de categorias
-            </Typography>
-        </Box>
-    );
+      <Typography variant="body1">
+        {/* Estrutura da tabela */}
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>ID</TableCell>
+                <TableCell>Nome</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableCell>1</TableCell>
+            <TableCell>Eletrônicoos</TableCell>
+          </Table>
+        </TableContainer>
+      </Typography>
+    </Box>
+  );
 }
